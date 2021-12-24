07ef84c5d4fc5d04c42d96347a260a4b7680b632
yesbirs56 <84576671+yesbirs56@users.noreply.github.com>
2021-12-24 16:23:15 +0530

    Add files via upload

---
+++ NUnitDemo/Calculator/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int? Div(int num1, int num2)
+        {
+            if (num2 == 0) return null;
+            return num1 / num2;
+        }
+    }
+}
